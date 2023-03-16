111d956e9110ed70bb356396c5c5e4ee156b469f
unknown <kashif_saleem_23@hotmail.com>
2023-03-16 21:33:07 +0500

    fix linter error

--- solver.rb
+++ solver.rb
@@ -2,13 +2,14 @@ class Solver
   def reverse(str)
     str.strip.reverse
   end
-  def factorial(n)
-    if n < 0 
-      raise ArgumentError, "Input must be 0 or a positive integer"
-    elsif n == 0 
-      return 1
+
+  def factorial(num)
+    if num.negative?
+      raise ArgumentError, 'Input must be 0 or a positive integer'
+    elsif num.zero?
+      1
     else
-      return (1..n).reduce(:*)
+      (1..num).reduce(:*)
     end
   end
 end
