80dfffdcbaddc0d7a266fce02d83e87e9b4704a8
unknown <kashif_saleem_23@hotmail.com>
2023-03-16 21:09:26 +0500

    Refactore code for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,24 +1,25 @@
 require 'rspec'
-require 'solver'
+require './solver'
 
-
-describe '#factorial' do
-  context 'when n is 0' do
-    it 'returns 1' do
-      expect(factorial(0)).to eq(1)
+RSpec.describe Solver do
+  describe '#factorial' do
+    context 'when n is 0' do
+      it 'returns 1' do
+        expect(subject.factorial(0)).to eq(1)
+      end
     end
-  end
 
-  context 'when n is a positive integer' do
-    it 'returns the correct factorial' do
-      expect(factorial(5)).to eq(120)
+    context 'when n is a positive integer' do
+      it 'returns the correct factorial' do
+        expect(subject.factorial(5)).to eq(120)
+      end
     end
-  end
 
-  context 'when n is a negative integer' do
-    it 'raises an ArgumentError' do
-      expect { factorial(-1) }.to raise_error(ArgumentError)
-      expect { factorial(-5) }. to raise_error(ArgumentError)
+    context 'when n is a negative integer' do
+      it 'raises an ArgumentError' do
+        expect { subject.factorial(-1) }.to raise_error(ArgumentError)
+        expect { subject.factorial(-5) }. to raise_error(ArgumentError)
+      end
     end
   end
 end
