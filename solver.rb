1e4c517f4c8151b76c74f518a7b57779aae7cdca
unknown <kashif_saleem_23@hotmail.com>
2023-03-16 20:55:38 +0500

    Refactor test for factorial

--- solver.rb
+++ solver.rb
@@ -1,2 +1,11 @@
 class Solver
+  
+  def factorial(N)
+    # Create a method called factorial that takes one argument, an integer N, and returns the factorial for that number. The factorial is the multiplication of all integers from 1 to N and has the special case that the factorial of 0 is 1. This method only accepts 0 and positive integers, so if a negative integer is given it should raise an exception.
+    i=1
+    while(i<=num)
+      fact=fact*i
+      i+=1
+    end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,10 +1,24 @@
 require 'rspec'
 require 'solver'
 
-describe Solver do
-  describe '#factorial(N)' do
-    it 'returns 1 when N is 0' do
-      expect(Solver.factorial(0)).to eq(1)
+
+describe '#factorial' do
+  context 'when n is 0' do
+    it 'returns 1' do
+      expect(factorial(0)).to eq(1)
     end
-   end
+  end
+
+  context 'when n is a positive integer' do
+    it 'returns the correct factorial' do
+      expect(factorial(5)).to eq(120)
+    end
+  end
+
+  context 'when n is a negative integer' do
+    it 'raises an ArgumentError' do
+      expect { factorial(-1) }.to raise_error(ArgumentError)
+      expect { factorial(-5) }. to raise_error(ArgumentError)
+    end
+  end
 end
